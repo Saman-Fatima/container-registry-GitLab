6875d5b7caab8cae16fe0fe5ba26518ef491769d
Saman-Fatima <73308738+Saman-Fatima@users.noreply.github.com>
2022-10-05 10:20:33 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+ls /home
 echo mypasswd | sudo -S chown nonroot /home/nonroot/
 echo mypasswd | sudo -S chown -R nonroot /opt/
 mkdir .ssh/
 echo mypasswd |  sudo cp -r /home/nonroot/ssh-mounted/ /home/nonroot/.ssh/
 echo mypasswd | sudo -S chown -R nonroot /home/nonroot/.ssh/
+cd /home/nonroot/
 exec "$@"
